64a08df27255365b7e067a8167202e3423bd7c48
Kenneth Francis <kenneth.w.francis@outlook.com>
2023-09-11 16:18:41 -0400

    completed assignment

---
+++ client/src/views/Main.js
@@ -0,0 +1,12 @@
+import ProductForm from '../components/ProductForm';
+
+const Main = () => {
+
+    return (
+        <div className="container col mt-5 mx-auto w-25">
+            <ProductForm />
+        </div>
+    )
+}
+
+export default Main;
